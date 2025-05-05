9f6cf2ebd6ee123b60603ad4804cfa37a0c5619d
bbering <bbering88@gmail.com>
2025-05-04 23:52:47 -0300

    feat(RabbitMQConfig): adicionado metodo para mapear jackson para json

--- src/main/java/com/ms/ms_email/configs/RabbitMQConfig.java
+++ src/main/java/com/ms/ms_email/configs/RabbitMQConfig.java
@@ -1,9 +1,12 @@
 package com.ms.ms_email.configs;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 public class RabbitMQConfig {
@@ -14,6 +17,12 @@ public class RabbitMQConfig {
     @Bean
     public Queue queue() {
         return new Queue(queue, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objMapper);
     }
 
 }
