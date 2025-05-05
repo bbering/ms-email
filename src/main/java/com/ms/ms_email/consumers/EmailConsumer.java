9894d71e4634cb6eb20eb1d52af203fde45ccea5
bbering <bbering88@gmail.com>
2025-05-04 23:51:19 -0300

    refactor(EmailConsumer): pequena refatoração do método listenEmailQueue para incluir como parâmetro um o DTO de criação de emails

--- src/main/java/com/ms/ms_email/consumers/EmailConsumer.java
+++ src/main/java/com/ms/ms_email/consumers/EmailConsumer.java
@@ -4,12 +4,14 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import com.ms.ms_email.dtos.EmailRequestDTO;
+
 @Component
 public class EmailConsumer {
 
     @RabbitListener(queues = "${broker.queue.email.name}")
-    public void listenEmailQueue(@Payload String string){
-        System.out.println(string);
+    public void listenEmailQueue(@Payload EmailRequestDTO emailData){
+        System.out.println(emailData.getEmailTo());
     }
     
 }
