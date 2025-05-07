af110087b82e9e81ea30b0ac33ce401ffb9473e1
bbering <bbering88@gmail.com>
2025-05-06 23:18:39 -0300

    refactor(EmailRequestDTO): refatorada DTO para criação de emails incluindo validações e renomeando variaveis

--- src/main/java/com/ms/ms_email/dtos/EmailRequestDTO.java
+++ src/main/java/com/ms/ms_email/dtos/EmailRequestDTO.java
@@ -2,14 +2,26 @@ package com.ms.ms_email.dtos;
 
 import java.util.UUID;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class EmailRequestDTO {
-    private UUID userId;
-    private String emailTo;
-    private String subject;
-    private String content;
+
+    @NotNull(message = "O ID do usuário é obrigatório.")
+    private UUID requesterUserId;
+
+    @NotBlank(message = "O e-mail de destino é obrigatório.")
+    @Email(message = "E-mail de destino inválido.")
+    private String destinationEmail;
+
+    @NotBlank(message = "O assunto do e-mail é obrigatório.")
+    private String messageSubject;
+
+    @NotBlank(message = "O conteúdo do e-mail é obrigatório.")
+    private String messageContent;
 }
