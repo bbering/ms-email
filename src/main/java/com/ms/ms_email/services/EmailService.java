7a5577ccf122f651e595f73cf34f1aafbc9fe1d4
bbering <bbering88@gmail.com>
2025-05-07 22:10:33 -0300

    feat(EmailService): criação do metodo auxiliar para mapear entidades e transforma-las em DTO

--- src/main/java/com/ms/ms_email/services/EmailService.java
+++ src/main/java/com/ms/ms_email/services/EmailService.java
@@ -3,6 +3,8 @@ package com.ms.ms_email.services;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import com.ms.ms_email.dtos.EmailRequestDTO;
+import com.ms.ms_email.models.Email;
 import com.ms.ms_email.repositories.EmailRepository;
 
 @Service
@@ -14,6 +16,16 @@ public class EmailService {
     public EmailService(EmailRepository emailRepository, JavaMailSender javaMailSender) {
         this.emailRepository = emailRepository;
         this.javaMailSender = javaMailSender;
+    }
+
+    public EmailRequestDTO toDTO(Email email) {
+        EmailRequestDTO entityConverted = new EmailRequestDTO();
+        entityConverted.setMessageContent(email.getContent());
+        entityConverted.setMessageSubject(email.getEmailSubject());
+        entityConverted.setRequesterUserId(email.getUserID());
+        entityConverted.setDestinationEmail(email.getEmailTo());
+
+        return entityConverted;
     }
 
 }
