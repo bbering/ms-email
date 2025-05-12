3ea2364cd8bed0db065dff30f90a0cb4b70f8c90
bbering <bbering88@gmail.com>
2025-05-11 22:50:07 -0300

    refactor(UserCreatedEventDTO): corrige mapeamento de userId para id

--- src/main/java/com/ms/ms_email/dtos/UserCreatedEventDTO.java
+++ src/main/java/com/ms/ms_email/dtos/UserCreatedEventDTO.java
@@ -2,12 +2,15 @@ package com.ms.ms_email.dtos;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserCreatedEventDTO {
+    @JsonProperty("userId")
     private UUID id;
     private String name;
     private String email;
