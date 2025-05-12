2b270d4e67acd6e924e4f66036aa2cb031461abb
bbering <bbering88@gmail.com>
2025-05-11 23:00:40 -0300

    refactor(UserCreatedEventDTO): ajustes na DTO para lidar com erros de desserialização jackson

--- src/main/java/com/ms/ms_email/dtos/UserCreatedEventDTO.java
+++ src/main/java/com/ms/ms_email/dtos/UserCreatedEventDTO.java
@@ -2,6 +2,7 @@ package com.ms.ms_email.dtos;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
@@ -9,10 +10,12 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserCreatedEventDTO {
     @JsonProperty("userId")
     private UUID id;
     private String name;
     private String email;
     private String emailTo;
+    private String subject;
 }
